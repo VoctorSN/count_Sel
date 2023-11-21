ff0be861c6854ddcb3edcdeb06d658b9ceeb7599
VoctorSN <144813362+VoctorSN@users.noreply.github.com>
2023-11-21 14:14:55 +0100

    Add files via upload

---
+++ count_sel/Test/test_once_values.py
@@ -0,0 +1,4 @@
+from src.count_sel import count_sel
+
+def test_once_values():
+    assert count_sel([-3, -2, -1, 3, 4, -5, -5, 5, -1, -5])[2]==5
---
+++ count_sel/src/count_sel.py
@@ -0,0 +1,19 @@
+def count_sel(lst):
+    once_values=0
+    numeros_aparecidos=[]
+    max_ocurrance=0
+    nb_max_ocurrance=[]
+    for num in lst:
+        if num in numeros_aparecidos:
+            continue
+        if lst.count(num)>max_ocurrance:
+            max_ocurrance=lst.count(num)
+            nb_max_ocurrance=[num]
+        if lst.count(num)==max_ocurrance and num not in nb_max_ocurrance:
+            nb_max_ocurrance.append(num)
+        if num not in numeros_aparecidos:
+            numeros_aparecidos.append(num)
+        if lst.count(num)==1:
+            once_values+=1
+    nb_max_ocurrance.sort()
+    return [len(lst),len(numeros_aparecidos),once_values,[nb_max_ocurrance,max_ocurrance]]
