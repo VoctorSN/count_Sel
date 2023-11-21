4dae34ef38a734e45c363b5a450f551db53f7895
VoctorSN <144813362+VoctorSN@users.noreply.github.com>
2023-11-21 14:26:20 +0100

    Update count_sel.py

--- src/count_sel.py
+++ src/count_sel.py
@@ -1,18 +1,18 @@
 def count_sel(lst):
     once_values=0
-    numeros_aparecidos=[]
+    appeared=[]
     max_ocurrance=0
     nb_max_ocurrance=[]
     for num in lst:
-        if num in numeros_aparecidos:
+        if num in appeared:
             continue
+        else:
+            numeros_aparecidos.append(num)
         if lst.count(num)>max_ocurrance:
             max_ocurrance=lst.count(num)
             nb_max_ocurrance=[num]
         if lst.count(num)==max_ocurrance and num not in nb_max_ocurrance:
             nb_max_ocurrance.append(num)
-        if num not in numeros_aparecidos:
-            numeros_aparecidos.append(num)
         if lst.count(num)==1:
             once_values+=1
     nb_max_ocurrance.sort()
